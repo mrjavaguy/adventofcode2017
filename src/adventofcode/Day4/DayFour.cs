1795a1954905ea1b74241cd4c93b2dca5aae57fb
Eric Holton <eholton@us.imshealth.com>
2017-12-04 07:41:27 -0800

    Day 4

---
+++ src/adventofcode/Day4/DayFour.cs
@@ -0,0 +1,43 @@
+﻿
+namespace AdventOfCode.Day4
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class DayFour
+    {
+
+        public static bool IsValidPassPhrase(string phrase)
+        {
+            return !GetWords(phrase).GroupBy(w => w).Any(g => g.Count() > 1);
+        }
+
+        public static string[] GetWords(this string phrase)
+        {
+            return phrase.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public static bool IsAnagram(string one, string two)
+        {
+            if (one.Length == two.Length)
+            {
+                return one.OrderBy(c => c).SequenceEqual(two.OrderBy(c => c));
+            }
+
+            return false;
+        }
+
+        public static bool ContainsAnagram(this IEnumerable<string> enumerable)
+        {
+            return enumerable.SelectMany(f => enumerable.Where(s => f != s).Select(s => (f, s))).Any(p => IsAnagram(p.f, p.s));
+        }
+
+
+        public static int StarOne(string[] input) =>
+             input.Where(s => IsValidPassPhrase(s)).Count();
+
+        public static int StarTwo(string[] input) =>
+            input.Where(s => !(s.GetWords().ContainsAnagram())).Where(s => IsValidPassPhrase(s)).Count();
+    }
+}
---
+++ test/adventofcodetests/Day4/DayFourTests.cs
@@ -0,0 +1,31 @@
+﻿namespace adventofcodetests.Day4
+{
+    using System.IO;
+    using Xunit;
+    using static AdventOfCode.Day4.DayFour;
+
+    public class DayFourTests
+    {
+        [Theory]
+        [InlineData("aa bb cc dd ee", true)]
+        [InlineData("aa bb cc dd aa", false)]
+        [InlineData("aa bb cc dd aaa", true)]
+        public void Day1PassPhraseTest(string input, bool expected)
+        {
+            var actual = IsValidPassPhrase(input);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("abcde fghi", false)]
+        [InlineData("abcde xyz ecdab", true)]
+        [InlineData("a ab abc abd abf abj", false)]
+        [InlineData("iiii oiii ooii oooi oooo", false)]
+        [InlineData("oiii ioii iioi iiio", true)]
+        public void Day1ContainsAnagramTest(string input, bool expected)
+        {
+            var actual = input.GetWords().ContainsAnagram();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
