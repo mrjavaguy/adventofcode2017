fefbf30418d335833e71567e86c763e98b9788a0
Eric Holton <eholton@us.imshealth.com>
2017-12-05 07:54:14 -0800

    Day 5

---
+++ src/adventofcode/Day5/DayFive.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Day5
+{
+    public static class DayFive
+    {
+        public static int JumpMaze(ImmutableList<int> input, Func<int, int> jumpRule)
+        {
+            var i = 0;
+            var l = input.Count;
+            var steps = 0;
+            while (i > -1 && i < l)
+            {
+                steps++;
+                var old = input[i];
+                input = input.SetItem(i, jumpRule(old));
+                i += old;
+            }
+            return steps;
+        }
+
+        public static int StarOne()
+        {
+            var input = File.ReadAllLines(@"C:\dev\personal\adventofcode2017\src\adventofcode\Day5\input.txt").Select(s => int.Parse(s)).ToImmutableList();
+            Func<int, int> rule = i => i + 1;
+            return JumpMaze(input, rule);
+        }
+
+        public static int StarTwo()
+        {
+            var input = File.ReadAllLines(@"C:\dev\personal\adventofcode2017\src\adventofcode\Day5\input.txt").Select(s => int.Parse(s)).ToImmutableList();
+            Func<int, int> rule = i => i >= 3 ? i - 1 : i + 1;
+            return JumpMaze(input, rule);
+        }
+    }
+}
---
+++ test/adventofcodetests/Day5/DayFiveTests.cs
@@ -0,0 +1,21 @@
+﻿
+namespace adventofcodetests.Day5
+{
+    using System;
+    using System.Collections.Immutable;
+    using System.IO;
+    using System.Linq;
+    using Xunit;
+    using static AdventOfCode.Day5.DayFive;
+
+    public class DayFiveTests
+    {
+        [Fact]
+        public void StarOneTests()
+        {
+            var input = ImmutableList<int>.Empty.Add(0).Add(3).Add(0).Add(1).Add(-3);
+            Func<int, int> rule = i => i + 1;
+            Assert.Equal(5, JumpMaze(input, rule));
+        }
+    }
+}
